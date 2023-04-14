329cd11e6a0e6336744009b5bd91fc20bffcd8e3
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2023-04-15 00:53:11 +0530

    Vercel Json

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-// // Require the necessary modules
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const path = require("path");
-// const db = require("./Db");
-// const authRouter = require("./routes/auth");
-// const projectRouter = require("./routes/project");
-
-// // Create a new express app
-// const app = express();
-
-// // Set up middleware
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// // Set up routes
-// app.use("/auth", authRouter);
-// app.use("/projects", projectRouter);
-
-// app.get("/", async (req, res) => {
-//   res.sendFile(path.join(__dirname, "./index.html"));
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+// Require the necessary modules
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const db = require("./Db");
+const authRouter = require("./routes/auth");
+const projectRouter = require("./routes/project");
 
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Set up routes
+app.use("/auth", authRouter);
+app.use("/projects", projectRouter);
 
 // Catch all handler for all other request.
 app.use("*", (req, res) => {
