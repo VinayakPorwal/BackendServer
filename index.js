84ead1c15c2f53c51f8a6ce49ecb91d786c0ad9e
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2023-04-15 00:50:11 +0530

    Vercel Json

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-// Require the necessary modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
-const db = require("./Db");
-const authRouter = require("./routes/auth");
-const projectRouter = require("./routes/project");
+// // Require the necessary modules
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const path = require("path");
+// const db = require("./Db");
+// const authRouter = require("./routes/auth");
+// const projectRouter = require("./routes/project");
 
-// Create a new express app
+// // Create a new express app
+// const app = express();
+
+// // Set up middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // Set up routes
+// app.use("/auth", authRouter);
+// app.use("/projects", projectRouter);
+
+// app.get("/", async (req, res) => {
+//   res.sendFile(path.join(__dirname, "./index.html"));
+// });
+
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const express = require("express");
 const app = express();
 
-// Set up middleware
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Set up routes
-app.use("/auth", authRouter);
-app.use("/projects", projectRouter);
-
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+// Catch all handler for all other request.
+app.use("*", (req, res) => {
+  res.json({ msg: "no route handler found" }).end();
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`index.js listening on ${port}`);
 });
