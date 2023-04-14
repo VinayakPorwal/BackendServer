93e62edfcd704d18655437e14816f26f801bfaf2
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2023-04-15 00:40:12 +0530

    Vercel Json

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use("/auth", authRouter);
 app.use("/projects", projectRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hey");
+});
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
